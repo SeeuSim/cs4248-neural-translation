9bfcbc24a0186a16ab0c5dc32998e743212c13ec
SeeuSim <seeusimong@gmail.com>
2024-04-05 14:34:00 +0800

    Add updated transformer training script
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- tokenisation/sentencepiece_custom/generate.py
+++ tokenisation/sentencepiece_custom/generate.py
@@ -5,22 +5,35 @@ from tqdm import tqdm
 
 def format_dataset():
     ds = load_dataset("iwslt2017", "iwslt2017-en-zh")
-    it = chain.from_iterable(
-        [
-            ds["train"],
-            ds["test"],
-            ds["validation"],
-        ]
-    )
+    # it = chain.from_iterable(
+    #     [
+    #         ds["train"],
+    #         ds["test"],
+    #         ds["validation"],
+    #     ]
+    # )
 
-    with open("./iwslt2017-en-zh.en", "w") as en_file, open(
-        "./iwslt2017-en-zh.zh", "w"
-    ) as zh_file:
-        for row in tqdm(map(lambda r: r["translation"], it)):
-            en_file.write(row["en"] + "\n")
-            zh_file.write(row["zh"] + "\n")
-        en_file.close()
-        zh_file.close()
+    for k in ["train", "test", "validation"]:
+        with open(f"./iwslt2017-en-zh-{k}.en", "w") as en_file, open(
+            f"./iwslt2017-en-zh-{k}.zh", "w"
+        ) as zh_file:
+            for row in tqdm(map(lambda r: r["translation"], ds[k])):
+                en_file.write(row["en"] + "\n")
+                zh_file.write(row["zh"] + "\n")
+            en_file.close()
+            zh_file.close()
+            
+        
+
+
+    # with open("./iwslt2017-en-zh.en", "w") as en_file, open(
+    #     "./iwslt2017-en-zh.zh", "w"
+    # ) as zh_file:
+    #     for row in tqdm(map(lambda r: r["translation"], it)):
+    #         en_file.write(row["en"] + "\n")
+    #         zh_file.write(row["zh"] + "\n")
+    #     en_file.close()
+    #     zh_file.close()
 
 
 if __name__ == "__main__":
