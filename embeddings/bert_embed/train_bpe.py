b0f1666d7a9100c9ccc29cbeb9c7bc59b23850da
SeeuSim <seeusimong@gmail.com>
2024-04-12 19:34:30 +0800

    Refactor embed-transformer scipt
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- embeddings/bert_embed/train_bpe.py
+++ embeddings/bert_embed/train_bpe.py
@@ -14,28 +14,43 @@ from transformers import (
 # Polyfill
 np.object = object
 
-IS_KAGGLE = False
+"""
+Variables to configure.
+"""
+DATASET_DISK_PATH = None  # Save the dataset using the Huggingface Datasets API save_to_disk, then load the file path.
+CUSTOM_TOKENISER_CLASS = None  # Import the class, then reference it here.
 
-IS_CUSTOM_TOK = False
+VOCAB_SIZE = 16_384
+TRAINER_OUTPUT_PATH = "./models"
 
-if IS_KAGGLE:
-    from kaggle_secrets import UserSecretsClient
-    import wandb
-
-    user_secrets = UserSecretsClient()
-    _WANDB_API_KEY = user_secrets.get_secret("wandb_sec")
-    wandb.login(key=_WANDB_API_KEY)
-
-    # upload the dataset to Kaggle first
-    dataset = datasets.load_from_disk("/kaggle/input/iwslt-en-zh/ds/")
+"""
+Dataset
+"""
+if DATASET_DISK_PATH is not None:
+    dataset = datasets.load_from_disk(DATASET_DISK_PATH)
 else:
     dataset = datasets.load_dataset("iwslt2017", "iwslt2017-en-zh")
 
-if IS_CUSTOM_TOK:
-    from ..utils import tokenise
+train, test = dataset["train"], dataset["test"]
 
-    # Add own class to output vocab indices, decode and attention mask
-    tokenizer = lambda sent: tokenise(sent)
+
+if CUSTOM_TOKENISER_CLASS is not None:
+    """
+    This tokeniser will have to implement:
+        - __call__(self, text: str | list[str], return_special_tokens_mask=False)
+            Given a sentence or a list of sentences, performs these steps for each sentence:
+                1. Tokenises it
+                2. Maps each token to a numeric ID within its learned vocabulary
+                3. Generates a dict of 4 equal length int arrays:
+                    - input_ids: The token IDs
+                    - attention_mask: An array of 1s for non-padding tokens, 0 for padding tokens
+                    - token_type_ids: 0s. We do not use this for the multi-context BERT task.
+                    - special_tokens_mask: 1s for BOS/EOS/PAD/UNK, 0 otherwise.
+        - __len__(self)
+            Returns the size of its learned vocabulary. Do add at least 4 for the 4 tokens: UNK, SOS, EOS, PAD.
+    """
+    # tokeniser = YourCustomTokeniser()
+    pass
 else:
     tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
 
@@ -55,11 +70,6 @@ torch.set_default_device(device)
 
 
 """
-Dataset
-"""
-train, test = dataset["train"], dataset["test"]
-
-"""
 Preprocess and tokenise
 """
 
@@ -76,15 +86,12 @@ train_dataset.set_format("torch")
 test_dataset = test.map(get_row_data, batched=True)
 test_dataset.set_format("torch")
 
+
 """
 Model Config
 """
-
-vocab_size = 16_384
-output_path = "./models"
-
 config = BertConfig(
-    vocab_size=vocab_size,
+    vocab_size=VOCAB_SIZE,
     max_position_embeddings=288,  # or 512 (sentence length for attn mask)
     hidden_size=256,
     # Add or modify other config parameters as needed
@@ -95,18 +102,18 @@ model = BertForMaskedLM(config)
 # Important: Tokenizer impls __len__ for output vocab size
 model.resize_token_embeddings(len(tokenizer))
 
+
 """
 Training Utilities
 """
-
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False,  # Disable Masked Language Modeling
 )
 
 training_args = TrainingArguments(
-    output_dir=output_path,
-    evaluation_strategy="steps",
+    output_dir=TRAINER_OUTPUT_PATH,
+    evaluation_strategy="steps",  # Eval by steps instead of epoch
     overwrite_output_dir=True,
     num_train_epochs=10,
     per_device_train_batch_size=10,
@@ -120,7 +127,6 @@ training_args = TrainingArguments(
     dataloader_pin_memory=False,
 )
 
-# Step 6: Create the trainer and start training
 trainer = Trainer(
     model=model,
     args=training_args,
@@ -129,13 +135,15 @@ trainer = Trainer(
     eval_dataset=test_dataset,
 )
 
+# Polyfill for weird MPS bug
 trainer._get_train_sampler = lambda: RandomSampler(
     trainer.train_dataset, generator=torch.Generator(device)
 )
+
 
 """
 Actual Training
 """
 if __name__ == "__main__":
     trainer.train()
-    trainer.save_model(output_path)
+    trainer.save_model(TRAINER_OUTPUT_PATH)
